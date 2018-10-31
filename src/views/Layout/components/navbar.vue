173303ff4545dd200a1241b13eeae088f574ef74
panhj <1159688878@qq.com>
2018-10-31 23:18:02 +0800

    debug for chrome

--- src/views/Layout/components/navbar.vue
+++ src/views/Layout/components/navbar.vue
@@ -4,16 +4,10 @@
     @select="handleSelect"  
     background-color="#545c64"
     text-color="#fff"
-    active-text-color="#ffd04b">
-        <el-menu-item index="1"><router-link tag="span" to="/home">处理中心</router-link></el-menu-item>
-        <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="3"><router-link to="/doc">消息中心</router-link></el-menu-item>
-        <el-menu-item index="4" disabled>消息中心2</el-menu-item>
+    active-text-color="#ffd04b"
+    :router="isroute">
+        <el-menu-item index="/home">处理中心</el-menu-item>
+        <el-menu-item index="/doc/index">消息中心</el-menu-item>
     </el-menu>
 </template>
 
@@ -22,7 +16,13 @@ export default {
     name: "navbar",
     data () {
         return {
-            activeIndex: '1'
+            // activeIndex: '/home'
+            isroute: true
+        }
+    },
+    computed: {
+        activeIndex: function () {
+            return this.$route.path;
         }
     },
     methods: {
